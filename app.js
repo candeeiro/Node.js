d358bded3c0cadeb7fe8345f1cd1f2e01e3fb922
candeeiro <50892851+candeeiro@users.noreply.github.com>
2023-08-19 22:58:24 -0300

    Node.js with MariaDB

---
+++ app.js
@@ -0,0 +1,23 @@
+const msg = 1; // constm let or var
+var msg2 = "mensagem 2";
+let msg3 = msg2;
+
+
+console.log(msg);
+console.log(msg2);
+console.log(msg3);
+
+console.log(msg3);
+console.log(msg+msg3);
+
+function fn()  { 
+    console.log(Pessoa)
+};
+const obj = { nome: "Manoel"   };
+
+class Pessoa {
+    name = "Manoel";
+    lastname = "Carvalho"
+}
+fn();
+lbl: console.log(1);
